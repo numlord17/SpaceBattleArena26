ccebfe4de544f922e835159316a7afd1d3ca873c
numlord17 <numlord17@gmail.com>
2026-05-28 09:06:33 -0700

    Update ExampleShip.java

--- ExampleShip.java
+++ ExampleShip.java
@@ -8,6 +8,7 @@ public class ExampleShip extends BasicSpaceship {
     private int worldWidth;
     private int worldHeight;
     private Point midpoint;
+    private ObjectStatus ship;
     public static void main(String[] args)
     {
         TextClient.run("10.56.98.121", new ExampleShip()); //default is 127.0.0.1
@@ -25,23 +26,70 @@ public class ExampleShip extends BasicSpaceship {
     @Override
     public ShipCommand getNextCommand(BasicEnvironment env)
     {
-        ObjectStatus ship = env.getShipStatus();
         
-        if (ship.getPosition().getAngleTo(midpoint) - ship.getOrientation() != 0)
+        ship = env.getShipStatus();
+        
+        String mode = "attack";
+        
+        if (mode.equals("findmiddle"))
         {
-            return new RotateCommand(ship.getPosition().getAngleTo(midpoint) - ship.getOrientation());
+            if (!isFacingCenter(env))
+            {
+                return new RotateCommand(ship.getPosition().getAngleTo(midpoint) - ship.getOrientation());
+            }
+            if (ship.getPosition().getDistanceTo(midpoint) >= 200)
+            {
+                return new ThrustCommand('B', 0.5, 1);
+            }
+            else if ((ship.getPosition().getDistanceTo(midpoint) < 200) && (isFacingCenter(env)))
+            {
+                return new BrakeCommand(0.01);
+            }
+            else
+            {
+                return new IdleCommand(0.1);
+            }
         }
-        else if (ship.getPosition().getDistanceTo(midpoint) >= 10)
+        if (mode.equals("attack"))
         {
-            return new ThrustCommand('B', 1, 1);
-        }
-        else if ((ship.getPosition().getDistanceTo(midpoint) < 10) && (ship.getSpeed() != 0))
-        {
-            return new AllStopCommand();
+            System.out.println(env.getRadar().getById(1499));
+            System.out.println(env.getRadar());
+            if (ship.getPosition().getDistanceTo(midpoint) >= 200)
+            {
+               return new RadarCommand(3,1499);
+            }
+            else
+            {
+               /*
+               if (!(env.getRadar().getById(1234).getPosition().getAngleTo(midpoint) - ship.getOrientation() == 0))
+               {
+                  return new RotateCommand(env.getRadar().getById(1234).getPosition().getAngleTo(midpoint) - ship.getOrientation());
+               }
+               else
+               {
+                  return new IdleCommand(0.1);
+               }
+               */
+               return new IdleCommand(0.1);
+            }
         }
         else
         {
             return new IdleCommand(0.1);
         }
+        
     }
+    
+    public boolean isFacingCenter(BasicEnvironment env)
+    {
+      if (ship.getPosition().getAngleTo(midpoint) - ship.getOrientation() == 0)
+      {
+          return true;
+      }
+      else
+      {
+         return false;
+      }
+    }
+    
 }
